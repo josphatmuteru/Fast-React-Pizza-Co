8348bc0c5e2b1ddb7be0e24fb395656b5eeb452b
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2023-07-25 16:59:03 +0300

    Styling with Tailwind CSS

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom';
+import LinkButton from '../../ui/LinkButton';
+import Button from '../../ui/Button';
+import CartItem from '../cart/CartItem';
 
 const fakeCart = [
   {
@@ -29,13 +32,21 @@ function Cart() {
 
   return (
     <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
-        <Link to="/order/new">Order pizzas</Link>
-        <button>Clear cart</button>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
+        <Button type="primary" to="/order/new">
+          Order pizzas
+        </Button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
