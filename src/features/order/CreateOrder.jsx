f82c856481a2e92cfaf4a764ce0dad740e7dc6fb
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2023-07-26 09:54:28 +0300

    Modelling state with Redux Toolkit, building the cart page, implementing cart CRUD operations

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -35,6 +36,7 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'loading';
+  const username = useSelector((state) => state.user.username);
 
   const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
@@ -42,12 +44,20 @@ function CreateOrder() {
 
   return (
     <div className="px-4 py-6">
-      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+      <h2 className="mb-8 text-xl font-semibold">
+        Ready to order? Let&apos;s go!
+      </h2>
 
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" required className="input grow" />
+          <input
+            type="text"
+            name="customer"
+            required
+            className="input grow"
+            defaultValue={username}
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-function Button({ disabled, children, to, type }) {
+function Button({ disabled, children, to, type, onClick }) {
   const base =
     'text-sm transtion-colors duration 300 active:bg-slate inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
   const styles = {
@@ -8,6 +8,7 @@ function Button({ disabled, children, to, type }) {
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
     secondary:
       'text-sm border-2 border-stone-300 px-4 py-2.5  md:px-6 md:py-3.5 transtion-colors duration 300  inline-block rounded-full  text-stone-400   hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:ring focus:ring-stone-200 focus:ring-offset-2 font-semibold uppercase tracking-wide focus:outline-none',
+    round: base + ' px-2.5 py-1  md:px-3.5 md:py-2 text-sm',
   };
 
   if (to)
@@ -15,6 +16,13 @@ function Button({ disabled, children, to, type }) {
       <Link to={to} className={styles[type]}>
         {children}
       </Link>
+    );
+
+  if (onClick)
+    return (
+      <button disabled={disabled} onClick={onClick} className={styles[type]}>
+        {children}
+      </button>
     );
 
   return (
